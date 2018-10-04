bcbca99fd68bd83c7a1f500c06261228d2435268
luca-morgese97 <luca.morgese@studenti.unitn.it>
2018-10-04 10:13:24 +0200

    correct req bug

--- api.js
+++ api.js
@@ -8,7 +8,7 @@ var courses_offered = [{id: 21, name: 'HCI'}, {id: 21, name: 'sweng'}];
 app.get('/', (req, res) => res.send('Hello, world!'));
 
 app.get('/courses', (req, res) => {
-	req.json(courses_offered);
+	res.json(courses_offered);
 });
 
 app.listen(PORT, () => console.log('Example app listening on port ' + PORT));
